64725e38d61a046e33c4447532166673e36887d3
Kostya Yegorov <kostyay@users.noreply.github.com>
2016-06-19 11:08:04 +0300

    Fix CSS

--- harim.shop.user.js
+++ harim.shop.user.js
@@ -27,23 +27,25 @@ function addJQuery(callback) {
 
 unsafeWindow.hideElements = function() {
 
-    items_table = jQ("#1itembody").closest("table")
+    //ad_rows
+    
+    items_table = jQ("#1itembody").closest("table");
     
     // hide the ads
     jQ(jQ("table[name=mainmenu]")[2]).css('display', 'none');
 
 	// expand the tables    
-    jQ("td[width=758]").attr("width", "100%")
-    main_table = jQ("table[width=894]")
-    jQ("td[width=120]").remove()
+    jQ("td[width=758]").attr("width", "100%");
+    main_table = jQ("table[width=894]");
+    jQ("td[width=120]").remove();
     
-    parents = jQ(".1reg").parents("table")
+    parents = jQ(".1reg").parents("table");
 	for (i = 0; i < parents.length; i++) { jQ(parents[i]).attr("width", "100%"); }    
     
-    jQ(main_table).attr("width", "90%")
-    jQ(main_table).attr("id", "main_table_element")
+    jQ(main_table).attr("width", "90%");
+    jQ(main_table).attr("id", "main_table_element");
     
-    jQ("#marq").closest("tr").remove()
+    jQ("#marq").closest("tr").remove();
 
     //jQ("td[colspan=13]").closest("tr").remove()
     //jQ(jQ("img[alt=099503332]").closest("tr")).remove()
@@ -54,20 +56,20 @@ unsafeWindow.hideElements = function() {
     //jQ(items_table).find("td:nth-child(11)").hide()
 }
 
-var originalShowHideItem = ShowHideItem
+var originalShowHideItem = ShowHideItem;
 function ShowHideItem_hook(id) {
     readItem(id);
         
-    originalShowHideItem(id)
+    originalShowHideItem(id);
 }
-ShowHideItem = ShowHideItem_hook
+ShowHideItem = ShowHideItem_hook;
 
 function readItem(id) {
     key = getKeyName(id);
     view_count = GM_getValue(key, 0);
     GM_setValue(key, view_count + 1);
     
-    GM_log('Setting ' + id + ' count to ' + GM_getValue(key, 0))
+    GM_log('Setting ' + id + ' count to ' + GM_getValue(key, 0));
     markItemReadStyle(itemById(id));
 }
 
@@ -76,17 +78,17 @@ function getKeyName(id) {
 }
 
 function wasItemRead(id) {
-    key = getKeyName(id)
-    GM_log('Viewcount for ' + id + ' is ' + GM_getValue(key, 0))
+    key = getKeyName(id);
+    GM_log('Viewcount for ' + id + ' is ' + GM_getValue(key, 0));
     return GM_getValue(key, 0) > 0;
 }
 
 function markItemReadStyle(elem) {
-    elem.css('font-weight', 'normal')
+    elem.css('font-weight', 'normal');
 }
 
 function markItemUnread(elem) {
-    elem.css('font-weight', 'bold')
+    elem.css('font-weight', 'bold');
 }
 
 function itemById(id) {
@@ -99,7 +101,7 @@ function markReadItems() {
     jQ("#main_table_element").prepend("<input type=\"button\" value=\"Mark all read\" onClick=\"javascript:markAllRead()\">");
     
     
-    items = jQ("[id^=TR\0-9+]");
+    items = jQ("[id^=TR]");
     for (i = 0; i < items.length; i++) {
         elem = jQ(items[i]);
         id = elem.attr('id').substring(2);
